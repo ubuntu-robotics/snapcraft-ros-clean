d59aeff63a2791666c9f27a65c339d59cb03e921
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2023-03-28 17:19:09 +0200

    remove rosunit

--- scripts/clean_ROS_debs.sh
+++ scripts/clean_ROS_debs.sh
@@ -2,16 +2,17 @@
 
 . clean_debs.sh
 
+remove-apt-package ros-$ROS_DISTRO-cmake-modules
+remove-apt-package ros-$ROS_DISTRO-gencpp
+remove-apt-package ros-$ROS_DISTRO-geneus
+remove-apt-package ros-$ROS_DISTRO-genmsg
+remove-apt-package ros-$ROS_DISTRO-gennodejs
+remove-apt-package ros-$ROS_DISTRO-genpy
 remove-apt-package ros-$ROS_DISTRO-message-generation
 remove-apt-package ros-$ROS_DISTRO-roslint
 remove-apt-package ros-$ROS_DISTRO-roslisp
 remove-apt-package ros-$ROS_DISTRO-rosmake
-remove-apt-package ros-$ROS_DISTRO-genpy
-remove-apt-package ros-$ROS_DISTRO-gennodejs
-remove-apt-package ros-$ROS_DISTRO-genmsg
-remove-apt-package ros-$ROS_DISTRO-geneus
-remove-apt-package ros-$ROS_DISTRO-gencpp
-remove-apt-package ros-$ROS_DISTRO-cmake-modules
+remove-apt-package ros-$ROS_DISTRO-rosunit
 remove-apt-package python3-pytest
 # remove globs of packages
 remove-apt-package-with-prefix ros-$DISTRO-catkin
